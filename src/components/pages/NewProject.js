6e344c87ea4d141d60c5e8793c607fee5b032c60
Bruno Staine <brunostaine@hotmail.com>
2022-07-10 17:31:40 -0300

    inserindo dados no db

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,13 +1,40 @@
 import './NewProject.css';
 
+import { useNavigate } from 'react-router-dom';
+
 import ProjectForm from '../projects/ProjectForm';
 
 function NewProject(){
+
+    var history = useNavigate()
+
+    function createPost(project) {
+
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application-json'
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            // redirect
+            history('/projects', {message: 'Projeto criado com sucesso!'})
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className="newproject_container">
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços.</p>
-            <ProjectForm btnText="Criar Projeto" />
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
         </div>
     )
 }
